d3ee57adb04c05dca824cc1f1e4b9d015ce4da34
Steven Andrews II <trey-94@hotmail.com>
2026-05-11 14:25:25 -0400

    Add files via upload

--- PocketDarkSouls/Speak/SpeakCharity.cs
+++ PocketDarkSouls/Speak/SpeakCharity.cs
@@ -14,32 +14,6 @@ public class SpeakCharity : Speak
 
     public void Execute(Player p1, Player p2)
     {
-        p1.Messenger.WarningMessage($"Speaking with : [ {p2.Name} : {p2.GetType()} ] ", ConsoleColor.Blue);
-        p1.Messenger.ReciveMessage(p2.Name,p2.DialogHandler.GenericSpeach(this.Dialog), ConsoleColor.Magenta);
-        while (true)  
-        {   
-            Console.WriteLine("Input gold total [ 0 to exit ]: ");
-            string input = Console.ReadLine();
-            if (int.TryParse(input, out int result))
-            {
-                if (result <= 0)
-                {
-                    p1.Messenger.ReciveMessage(p2.Name,p2.DialogHandler.BadCharity(this.Dialog, 0), ConsoleColor.Magenta);
-                    break;
-                }
-                else
-                {
-                    if (!p1.Wallet.GiveGold(result)) {
-                        p1.Messenger.ErrorMessage("You shouldnt be making enemies here...", ConsoleColor.Red);
-                        p1.Messenger.ReciveMessage(p2.Name,p2.DialogHandler.BadCharity(this.Dialog,1), ConsoleColor.Magenta);
-                        break;
-                    }
-                    p2.Wallet.AddGold(result);
-                    p1.Messenger.ReciveMessage(p2.Name,p2.DialogHandler.ThankYouSpeach(this.Dialog), ConsoleColor.Magenta);
-                    break;
-                }
-            }
-
-        }
+       p1.CharityMenu(p2)
     }
 }
